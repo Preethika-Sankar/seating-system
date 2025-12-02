91767adb4f79ce44e291e0e5024c3817cf47235f
Preethika Sankar <preethikasankar2005@gmail.com>
2025-12-02 23:37:34 +0530

    Replace full SQL code with corrected student data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,4 @@
--- Exams
-INSERT INTO exam (name) VALUES ('Exam 2');
-INSERT INTO exam (name) VALUES ('Exam 4');
-INSERT INTO exam (name) VALUES ('Exam 6');
-INSERT INTO exam (name) VALUES ('Exam 7');
-INSERT INTO exam (name) VALUES ('Exam 8');
-
--- Rooms
-INSERT INTO room (name) VALUES ('A01');
-INSERT INTO room (name) VALUES ('A02');
-INSERT INTO room (name) VALUES ('A03');
-INSERT INTO room (name) VALUES ('A04');
-INSERT INTO room (name) VALUES ('A05');
-INSERT INTO room (name) VALUES ('B01');
-INSERT INTO room (name) VALUES ('B02');
-INSERT INTO room (name) VALUES ('B03');
-INSERT INTO room (name) VALUES ('B04');
-INSERT INTO room (name) VALUES ('B05');
-INSERT INTO room (name) VALUES ('C01');
-INSERT INTO room (name) VALUES ('C02');
-INSERT INTO room (name) VALUES ('C03');
-
--- Students (name, rollNo, course, semester)
+-- Insert students with correct column names
 INSERT INTO student (name, roll_no, course, semester) VALUES ('Preethika', 'A01', 'CSE', 7);
 INSERT INTO student (name, roll_no, course, semester) VALUES ('Gomathi',   'A02', 'CSE', 7);
 INSERT INTO student (name, roll_no, course, semester) VALUES ('Akalya',    'A03', 'AIDS', 7);
