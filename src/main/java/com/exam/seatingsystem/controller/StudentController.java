2b41527b622bf8eb163dfa2066bc55440675bcaa
Preethika Sankar <preethikasankar2005@gmail.com>
2025-12-03 11:32:01 +0530

    Add /students endpoint for frontend

---
+++ src/main/java/com/exam/seatingsystem/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.exam.seatingsystem.controller;
+
+import com.exam.seatingsystem.model.Student;
+import com.exam.seatingsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
